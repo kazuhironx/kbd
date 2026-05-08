82b42aa25a97ae2241ee272c19bcb04971eb7029
kazuhiro-monden <kazuhiro.monden@sony-honda-mobility.com>
2026-05-09 08:53:49 +0900

    Update QMK scaffold for current keyboard json

--- firmware/qmk/kazuhironx_split60_ortho/config.h
+++ firmware/qmk/kazuhironx_split60_ortho/config.h
@@ -1,10 +1,3 @@
 #pragma once
 
-// Draft pinout. Confirm against the KiCad schematic before building.
-#define MATRIX_ROW_PINS { D1, D0, D4, C6, D7 }
-#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3 }
-
-#define DIODE_DIRECTION COL2ROW
-#define SOFT_SERIAL_PIN D2
-
 #define SPLIT_USB_DETECT
